f8eac1dc2939f54a1d4ec5800cc652e0079b50bb
dimond72721 <dimond14122009@gmail.com>
2026-08-02 22:17:57 +0300

    Refactor Flask app structure and add blueprints
    Refactor app initialization and structure, adding blueprints for auth and tasks.

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask
+from config import Config
+from extensions import db, login_manager
 
-app = Flask(__name__)
 
-tasks = []
+from models import User
 
-@app.route("/")
-def index():
-    return render_template("index.html", tasks=tasks)
+
+from auth.routes import auth
+from tasks.routes import tasks
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+
+    app.register_blueprint(auth)
+    app.register_blueprint(tasks)
+
+    
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
